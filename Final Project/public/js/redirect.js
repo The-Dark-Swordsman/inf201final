8ad946cf0b69a08a5d9425cdf766058b948e3ce6
Dark Swordsman <kylebarr54321@gmail.com>
2017-05-09 17:08:24 -0400

    adding comments to redirect.js

--- Final Project/public/js/redirect.js
+++ Final Project/public/js/redirect.js
@@ -1,15 +1,16 @@
 var time = 5;
 
 document.getElementById("redirect").innerHTML = "You Will Be Automatically Redirected In: " + time;
-
+// button to stop the redirect
 var stop = false;
 document.getElementById("stopRedirect").onclick = function(){
   stop = true;
   document.getElementById("stopText").innerHTML = "Stopped";
 }
-
+// the redirect function
 function redirect(){
   if(time == 0){
+    // if the button was clicked, stop the redirect
     if(stop == false){
       window.location = "http://www.albany.edu/~kb366566/final/home.html";
     }
@@ -20,9 +21,9 @@ function redirect(){
     }
   }
 }
-
+// call the redirect function every 1second (1000 ms).
 setInterval(redirect, 1000);
-
+// Get the name from localStorage and tell the user Thank You for submitting
 document.getElementById("thanks").innerHTML = "Thank You, " + localStorage.name + ", for Submitting your Form!"
 
 console.log("Redirect Loaded");
